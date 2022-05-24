443067b541c156c40ce1c57fe5e72e6437a87d6a
DoDayum <970617Baiyu!>
2022-05-24 21:07:00 +0800

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+
+    char demoInt [100];
+    for (int i = 0; i < sizeof(demoInt) / sizeof(demoInt[0]); ++i){
+        demoInt[i] = 'hello';
+    }
+
     printf("Hello, World!\n");
     return 0;
 }
